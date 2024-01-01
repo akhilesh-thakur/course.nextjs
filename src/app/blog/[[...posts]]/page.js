29dbcc65bf30e34846e5a8ffb1bf46b3bf58a8d7
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2024-01-01 13:00:37 +0530

    Done with routings

---
+++ src/app/blog/[[...posts]]/page.js
@@ -0,0 +1,7 @@
+export default function page({ params }) {
+    return (
+      <>
+      <div>This is a Dynamic Catch all Blog page</div>
+      </>
+    )
+  }
---
+++ src/app/profile/[username]/page.js
@@ -0,0 +1,3 @@
+export default function page({ params }) {
+  return <div>Username: {params.username}</div>;
+}
