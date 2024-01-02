043da03462a3bcf26aff778008ba9189e01800af
Akhilesh <53186185+akhilesh-thakur@users.noreply.github.com>
2024-01-02 12:05:44 +0530

    Styling main container with custom CSS in globals.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
+        <div className="Main">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
